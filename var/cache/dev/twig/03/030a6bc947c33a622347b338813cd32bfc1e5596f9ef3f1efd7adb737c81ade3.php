380115034a745bf305ab539ff841e1b124a28b16
broham <durangolm85@gmail.com>
2017-04-17 22:48:33 -0400

    put in data to display

--- var/cache/dev/twig/03/030a6bc947c33a622347b338813cd32bfc1e5596f9ef3f1efd7adb737c81ade3.php
+++ var/cache/dev/twig/03/030a6bc947c33a622347b338813cd32bfc1e5596f9ef3f1efd7adb737c81ade3.php
@@ -21,65 +21,86 @@ class __TwigTemplate_bb0ee2b881a0e5accbefba378da2b3706906c5af348508e76379ddf80b4
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_5ebf7efff9a6896ac7b9e913a1eb1aa30f22b260acc7de795411c920d576c272 = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
-        $__internal_5ebf7efff9a6896ac7b9e913a1eb1aa30f22b260acc7de795411c920d576c272->enter($__internal_5ebf7efff9a6896ac7b9e913a1eb1aa30f22b260acc7de795411c920d576c272_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "MauroTrainingBundle:performance:index.html.twig"));
+        $__internal_6423da2ac574067fb94589d7e7e1a1c278b2260f332586432ce00d57c0a245ed = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_6423da2ac574067fb94589d7e7e1a1c278b2260f332586432ce00d57c0a245ed->enter($__internal_6423da2ac574067fb94589d7e7e1a1c278b2260f332586432ce00d57c0a245ed_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "MauroTrainingBundle:performance:index.html.twig"));
 
-        $__internal_3096e3e57393797fd862fddeb70c657f5cf8ee0a8b9f5241a51ad06a7279dab2 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
-        $__internal_3096e3e57393797fd862fddeb70c657f5cf8ee0a8b9f5241a51ad06a7279dab2->enter($__internal_3096e3e57393797fd862fddeb70c657f5cf8ee0a8b9f5241a51ad06a7279dab2_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "MauroTrainingBundle:performance:index.html.twig"));
+        $__internal_43ccdf14e2844355739272af9046441b2a237a30b2ac6d91e4c816c6ad404c82 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_43ccdf14e2844355739272af9046441b2a237a30b2ac6d91e4c816c6ad404c82->enter($__internal_43ccdf14e2844355739272af9046441b2a237a30b2ac6d91e4c816c6ad404c82_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "MauroTrainingBundle:performance:index.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_5ebf7efff9a6896ac7b9e913a1eb1aa30f22b260acc7de795411c920d576c272->leave($__internal_5ebf7efff9a6896ac7b9e913a1eb1aa30f22b260acc7de795411c920d576c272_prof);
+        $__internal_6423da2ac574067fb94589d7e7e1a1c278b2260f332586432ce00d57c0a245ed->leave($__internal_6423da2ac574067fb94589d7e7e1a1c278b2260f332586432ce00d57c0a245ed_prof);
 
         
-        $__internal_3096e3e57393797fd862fddeb70c657f5cf8ee0a8b9f5241a51ad06a7279dab2->leave($__internal_3096e3e57393797fd862fddeb70c657f5cf8ee0a8b9f5241a51ad06a7279dab2_prof);
+        $__internal_43ccdf14e2844355739272af9046441b2a237a30b2ac6d91e4c816c6ad404c82->leave($__internal_43ccdf14e2844355739272af9046441b2a237a30b2ac6d91e4c816c6ad404c82_prof);
 
     }
 
     // line 3
     public function block_content($context, array $blocks = array())
     {
-        $__internal_9944e9d08191c155e80ee2c7eb6b8b2d7bd4af7c28894a973438170fdcd50b46 = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
-        $__internal_9944e9d08191c155e80ee2c7eb6b8b2d7bd4af7c28894a973438170fdcd50b46->enter($__internal_9944e9d08191c155e80ee2c7eb6b8b2d7bd4af7c28894a973438170fdcd50b46_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "content"));
+        $__internal_5b9aae4fccf1c70767793d20cb8c22918398980ffc44a4baa2e56f298a65be8e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_5b9aae4fccf1c70767793d20cb8c22918398980ffc44a4baa2e56f298a65be8e->enter($__internal_5b9aae4fccf1c70767793d20cb8c22918398980ffc44a4baa2e56f298a65be8e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "content"));
 
-        $__internal_f5d6b495d89d54d73adae5af4749c8ae94646482e1de86069f044073d9136ca6 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
-        $__internal_f5d6b495d89d54d73adae5af4749c8ae94646482e1de86069f044073d9136ca6->enter($__internal_f5d6b495d89d54d73adae5af4749c8ae94646482e1de86069f044073d9136ca6_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "content"));
+        $__internal_01fd0e9415d453f0cbed12cb921c3ce3b3be5158ae7e53cbd704f9d5c242ff07 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_01fd0e9415d453f0cbed12cb921c3ce3b3be5158ae7e53cbd704f9d5c242ff07->enter($__internal_01fd0e9415d453f0cbed12cb921c3ce3b3be5158ae7e53cbd704f9d5c242ff07_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "content"));
 
         // line 4
-        echo "    ";
+        echo "    <table class=\"table table-hover table-border table_results\">
+        <thead>
+            <tr>
+                <th>Date of Workout</th>
+                <th>Result</th>
+                <th>workout Description</th>
+                <th>Date of Workout</th>
+                <th>Measurement_units</th>
+                <th>performance</th>
+            </tr>
+            <th>
+
+            </th>
+
+        </thead>
+        <tbody>
+            <tr>
+            ";
+        // line 21
         $context['_parent'] = $context;
-        $context['_seq'] = twig_ensure_traversable((isset($context["performances"]) || array_key_exists("performances", $context) ? $context["performances"] : (function () { throw new Twig_Error_Runtime('Variable "performances" does not exist.', 4, $this->getSourceContext()); })()));
+        $context['_seq'] = twig_ensure_traversable((isset($context["performances"]) || array_key_exists("performances", $context) ? $context["performances"] : (function () { throw new Twig_Error_Runtime('Variable "performances" does not exist.', 21, $this->getSourceContext()); })()));
         foreach ($context['_seq'] as $context["_key"] => $context["performance"]) {
-            // line 5
-            echo "        <li>";
+            // line 22
+            echo "                <li>";
             echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), $context["performance"], "date_of_workout", array()), "html", null, true);
             echo "</li>
-        <li>";
-            // line 6
+                <li>";
+            // line 23
             echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), $context["performance"], "result", array()), "html", null, true);
             echo "</li>
-        <li>";
-            // line 7
+                <li>";
+            // line 24
             echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), $context["performance"], "workout_description", array()), "html", null, true);
             echo "</li>
-        <li>";
-            // line 8
+                <li>";
+            // line 25
             echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), $context["performance"], "measurement_units", array()), "html", null, true);
             echo "</li>
-        <li>";
-            // line 9
+                <li>";
+            // line 26
             echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), $context["performance"], "performance_type", array()), "html", null, true);
-            echo "</li>
-    ";
+            echo "</li></table>
+            ";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['performance'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 28
+        echo "        </tbod  y>
+";
         
-        $__internal_f5d6b495d89d54d73adae5af4749c8ae94646482e1de86069f044073d9136ca6->leave($__internal_f5d6b495d89d54d73adae5af4749c8ae94646482e1de86069f044073d9136ca6_prof);
+        $__internal_01fd0e9415d453f0cbed12cb921c3ce3b3be5158ae7e53cbd704f9d5c242ff07->leave($__internal_01fd0e9415d453f0cbed12cb921c3ce3b3be5158ae7e53cbd704f9d5c242ff07_prof);
 
         
-        $__internal_9944e9d08191c155e80ee2c7eb6b8b2d7bd4af7c28894a973438170fdcd50b46->leave($__internal_9944e9d08191c155e80ee2c7eb6b8b2d7bd4af7c28894a973438170fdcd50b46_prof);
+        $__internal_5b9aae4fccf1c70767793d20cb8c22918398980ffc44a4baa2e56f298a65be8e->leave($__internal_5b9aae4fccf1c70767793d20cb8c22918398980ffc44a4baa2e56f298a65be8e_prof);
 
     }
 
@@ -95,7 +116,7 @@ class __TwigTemplate_bb0ee2b881a0e5accbefba378da2b3706906c5af348508e76379ddf80b4
 
     public function getDebugInfo()
     {
-        return array (  71 => 9,  67 => 8,  63 => 7,  59 => 6,  54 => 5,  49 => 4,  40 => 3,  11 => 1,);
+        return array (  97 => 28,  89 => 26,  85 => 25,  81 => 24,  77 => 23,  72 => 22,  68 => 21,  49 => 4,  40 => 3,  11 => 1,);
     }
 
     public function getSourceContext()
@@ -103,13 +124,31 @@ class __TwigTemplate_bb0ee2b881a0e5accbefba378da2b3706906c5af348508e76379ddf80b4
         return new Twig_Source("{% extends '@MauroTraining/layout.html.twig' %}
 
 {% block content %}
-    {% for performance in performances %}
-        <li>{{ performance.date_of_workout }}</li>
-        <li>{{ performance.result }}</li>
-        <li>{{ performance.workout_description }}</li>
-        <li>{{ performance.measurement_units }}</li>
-        <li>{{ performance.performance_type }}</li>
-    {% endfor %}
+    <table class=\"table table-hover table-border table_results\">
+        <thead>
+            <tr>
+                <th>Date of Workout</th>
+                <th>Result</th>
+                <th>workout Description</th>
+                <th>Date of Workout</th>
+                <th>Measurement_units</th>
+                <th>performance</th>
+            </tr>
+            <th>
+
+            </th>
+
+        </thead>
+        <tbody>
+            <tr>
+            {% for performance in performances %}
+                <li>{{ performance.date_of_workout }}</li>
+                <li>{{ performance.result }}</li>
+                <li>{{ performance.workout_description }}</li>
+                <li>{{ performance.measurement_units }}</li>
+                <li>{{ performance.performance_type }}</li></table>
+            {% endfor %}
+        </tbod  y>
 {% endblock %}", "MauroTrainingBundle:performance:index.html.twig", "/home/maxpowers/Public/mauro9653/src/Mauro/TrainingBundle/Resources/views/performance/index.html.twig");
     }
 }
